133df0f23271f46554d3964265b244ab180473ea
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-06-27 17:41:13 +0530

    Removed Previous Tracks

--- frontend/components/AreYouReady.jsx
+++ frontend/components/AreYouReady.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import React, { useContext } from 'react'
+import { useRef } from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { AiFillVideoCamera, AiFillAudio } from 'react-icons/ai'
@@ -11,11 +12,13 @@ export default function AreYouReady({socket}) {
     
     const {setjoinroom,roomID,localStream,auth,video,setvideo,audio,setaudio} = useContext(Context)
     const router = useRouter()
+    const ReadyStream = useRef()
     // we are going to make an api call to check if the room is present or not
     //if the room is present .filter(room_id = room_id)
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ video:true, audio:true }).then((stream) => {
             localStream.current = stream
+            ReadyStream.current = stream
             let video = document.getElementById('my_video');
             video.srcObject = stream;
             video.onloadeddata = () => {
@@ -44,6 +47,7 @@ export default function AreYouReady({socket}) {
             <div className='backdrop-blur-[1px] w-full items-center h-full flex justify-center'>
                     <button onClick={()=>{
                         setjoinroom(false)
+                        ReadyStream.current.getTracks().forEach(track => track.stop())
                         router.push(`/RoomPage/${router.query.EachRoom}`)
                     }} className='bg-blue-600 text-lg lg:text-2xl text-white hover:bg-blue-600/90 transition-all fade-in-out px-4 p-2 lg:p-4 rounded-full  h-fit w-fit my-5  lg:my-auto hover:scale-110'>Are You Ready To Join ??</button>
             </div>
--- frontend/utils/GetLocalPreviewAndInitRoomConnection.jsx
+++ frontend/utils/GetLocalPreviewAndInitRoomConnection.jsx
@@ -2,20 +2,18 @@ import useWindowSize from "@rooks/use-window-size"
 import axios from "axios"
 import { createNewRoom } from "./CreateNewRoom"
 import { JoinRoom } from "./JoinRoom"
-import { fetchTurnCredentials, getTurnIceServers } from "./TurnServers"
-
+const defaultControls = {
+    audio: true,
+    video: {
+        aspectRatio: { ideal: 3 / 1 } // Set the desired aspect ratio (width:height)
+    }
+};
 
 
 export const getLocalPreviewAndInitRoomConnection = (socket, localStream, isRoomHost, auth, roomID, setoverlay, title, IceServers,number) => {
-    const defaultControls = {
-        audio: true,
-        video: {
-            aspectRatio: { ideal: 3 / 1 } // Set the desired aspect ratio (width:height)
-        }
-    };
+    
     
     navigator.mediaDevices.getUserMedia(defaultControls).then((stream) => {
-        var VideoTrack = stream.getVideoTracks()[0]
         localStream.current = stream;
         setoverlay(false)
         axios.post('https://www.pradeeps-video-conferencing.store/api/v1/UserStatus/', { roomID: roomID }, {
@@ -34,7 +32,7 @@ export const getLocalPreviewAndInitRoomConnection = (socket, localStream, isRoom
 
     }).catch(err => {
         console.log(err)
-        alert('error in navigatore.mediaDevices')
+        alert(err)
     })
 
     // fetchTurnCredentials().then((response)=>{
