246b35c4f5ca321ea20e20a0fea6c493255ff528
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-07-26 23:40:05 +0530

    Added web manifest

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -7,7 +7,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <head>
-        
+      <link rel="manifest" href="/manifest.webmanifest"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#ffc40d" />
+          <meta name="theme-color" content="#0D5BE1" />
       <meta property="twitter:image" content={imageDirectURL} />
         <meta property="twitter:title" content="ConferoLive - Meet Live" />
         <meta property="twitter:description" content="ConferoLive - Meet Live" />
