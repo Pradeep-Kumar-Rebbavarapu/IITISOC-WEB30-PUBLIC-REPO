d8e2717c38af34f65a42dbdd4fed1cbe3c524afc
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-07-26 22:15:10 +0530

    final touch up 4

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -1,33 +1,31 @@
 import { store } from '@/store/store'
 import { Html, Head, Main, NextScript } from 'next/document'
 import { useEffect } from 'react'
-
+import imageDirectURL from '../public/images/logo.png'
 export default function Document() {
 
   return (
     <Html lang="en">
       <head>
         
-        <meta property="twitter:image" content="https://drive.google.com/file/d/1jBGW4IqOp00Hqjs4SZJTf5Aap3d58xeL/view?usp=drive_link" />
-        <meta property="twitter:title" content="The Cynaptics Club - IIT Indore" />
-        <meta property="twitter:description" content="The Cynaptics Club(AI/ML) - IIT INDORE" />
+      <meta property="twitter:image" content={imageDirectURL} />
+        <meta property="twitter:title" content="ConferoLive - Meet Live" />
+        <meta property="twitter:description" content="ConferoLive - Meet Live" />
 
-        
-        <meta property="og:title" content="The Cynaptics Club - IIT Indore" />
-        <meta property="og:description" content="The Cynaptics Club(AI/ML) - IIT INDORE" />
-        <meta property="og:image" content="https://drive.google.com/file/d/1jBGW4IqOp00Hqjs4SZJTf5Aap3d58xeL/view?usp=drive_link" />
+        <meta property="og:title" content="ConferoLive - Meet Live" />
+        <meta property="og:description" content="ConferoLive - Meet Live" />
+        <meta property="og:image" content={imageDirectURL} />
         <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" /> 
-        <meta property="og:image:height" content="630" /> 
-        <meta property="og:image:alt" content="The Cynaptics Club - IIT Indore" /> 
-        <meta property="og:type" content="website" /> 
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="ConferoLive - Meet Live" />
+        <meta property="og:type" content="website" />
         <meta property="og:url" content={process.env.SITE_URL} />
 
-        
-        <meta name="description" content="The Cynaptics Club(AI/ML) - IIT INDORE" />
-        <meta itemprop="name" content="The Cynaptics Club - IIT Indore" />
-        <meta itemprop="description" content="The Cynaptics Club(AI/ML) - IIT INDORE" />
-        <meta itemprop="image" content="https://drive.google.com/file/d/1jBGW4IqOp00Hqjs4SZJTf5Aap3d58xeL/view?usp=drive_link" />
+        <meta name="description" content="ConferoLive - Meet Live" />
+        <meta itemprop="name" content="ConferoLive - Meet Live" />
+        <meta itemprop="description" content="ConferoLive - Meet Live" />
+        <meta itemprop="image" content={imageDirectURL} />
         <meta name="keywords" content="Video Conferencing ConferoLive Next js Django Django Rest Framework" />
         <meta name="author" content="Your Name" />
         <meta name="robots" content="index, follow" />
